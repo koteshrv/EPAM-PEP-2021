94a7f34eda2dc1c62531c9c7af22f90fccacc70f
koteshrv <kkotesh100@gmail.com>
2021-02-01 01:35:20 +0530

    Maven Home task

---
+++ Practice/maven-home-task/src/main/java/com/epam/hometask/App.java
@@ -0,0 +1,13 @@
+package com.epam.hometask;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
